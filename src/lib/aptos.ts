cbc5e37003fb8c1193cf8a9e43113e181e03206f
Tarun Sharma <sharmatarun1019@gmail.com>
2024-08-28 20:20:22 +0530

    feat: aptos init

---
+++ src/lib/aptos.ts
@@ -0,0 +1,18 @@
+import { AccountAddress, Aptos, AptosConfig, NetworkToNetworkName } from "@aptos-labs/ts-sdk";
+import { Network } from "aptos";
+
+
+const APTOS_NETWORK: Network = Network.TESTNET;
+const config = new AptosConfig({ network: APTOS_NETWORK });
+const aptos = new Aptos(config);
+
+
+export const getBalance = async (
+    accountAddress: AccountAddress,
+  ): Promise<number> => {
+    const amount = await aptos.getAccountAPTAmount({
+      accountAddress,
+    });
+    console.log(`balance is: ${amount}`);
+    return amount;
+  };
